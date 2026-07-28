39c8b0fa951bbfeb3787ca4ed2df52aa26d07ad1
Fauxly <tailormoon@icloud.com>
2026-07-28 22:36:55 +0300

    ci: rebuild macOS artifacts

--- src/libirecovery.c
+++ src/libirecovery.c
@@ -3370,27 +3370,10 @@ static irecv_error_t irecv_send_command_raw(irecv_client_t client, const char* c
 	}
 
 	if (length > 0) {
-    int ret = irecv_usb_control_transfer(
-        client,
-        0x40,
-        b_request,
-        0,
-        0,
-        (unsigned char*)command,
-        length + 1,
-        USB_TIMEOUT);
+		irecv_usb_control_transfer(client, 0x40, b_request, 0, 0, (unsigned char*) command, length + 1, USB_TIMEOUT);
+	}
 
-    debug("USB CMD='%s' bRequest=%u ret=%d\n",
-          command,
-          b_request,
-          ret);
-
-    if (ret < 0) {
-        return IRECV_E_USB_UPLOAD;
-    }
-}
-
-return IRECV_E_SUCCESS;
+	return IRECV_E_SUCCESS;
 }
 #endif
 
